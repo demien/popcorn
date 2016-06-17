649efee1fab74c5889f27bfd2b824872f4dcb290
demien <demien@appannie.com>
2016-06-17 14:34:56 +0800

    WIP

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as requirements:
+    requires = [i.split('=')[0].strip() for i in requirements.readlines() if i]
+
 
 setup(
     name='popcorn',
@@ -9,6 +12,8 @@ setup(
     long_description=open('README.rst').read(),
     url='https://github.com/demien-aa/popcorn',
     license='BSD',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    install_requires=requires,
     entry_points={
         'console_scripts': [
             'popcorn = popcorn.__main__:main',
