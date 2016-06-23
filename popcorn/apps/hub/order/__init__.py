1bc4e312bdd63668664508e1a39f9a3a54a70a08
demien <demien@appannie.com>
2016-06-23 13:24:42 +0800

    add instructino & order

--- popcorn/apps/hub/order/__init__.py
+++ popcorn/apps/hub/order/__init__.py
@@ -2,5 +2,11 @@ from instruction import WorkerInstruction
 
 
 class Order(object):
-    pass
+
+    def __init__(self):
+        self.instructions = list
+
+    def add_instruction(self, type, instruction):
+        new_instruction = Instruction.create(type, instruction)
+        self.instructions.append(new_instruction)
 
--- popcorn/apps/hub/order/instruction.py
+++ popcorn/apps/hub/order/instruction.py
@@ -1,21 +1,50 @@
 import abc
+import re
+from celery.utils.imports import instantiate
+
+
+
+class InstructionType(object):
+    WORKER = 'worker'
+
+
+instruction_mapping = {
+    InstructionType.WORKER: 'popcorn.apps.hub.order.instruction.WorkerInstruction',
+}
 
 
 class Instruction(object):
-
     __metaclass__ = abc.ABCMeta
+    SEPERATOR = ':'
 
-    def foo(self, **kwargs):
+    @staticmethod
+    def create(type, instruction):
+        return instantiate(instruction_mapping[type], instruction)
+
+    def parse(self, instruction):
         raise NotImplementedError()
+
+
+class Operator(object):
+    TO = '='
+    INC = '+'
+    DEC = '-'
+    ALL = [TO, INC, DEC]
 
 
 class WorkerInstruction(Instruction):
 
-    def __init__(self, instruction=None):
+    def __init__(self, instruction):
+        self.queue = None
+        self.worker_cnt = None
+        self.operator = None
         if instruction:
-            self._parse(instruction)
+            self.queue, self.operator, self.worker_cnt = self.parse(instruction)
+        self.opcodes = (self.queue, self.operator, self.worker_cnt)
 
-    def _parse(self, instruction):
+    def parse(self, instruction):
         """
         :param instruction. Example: 'abc:+1' means queue abc add one worker. 'abc:=1 means queue abc have one worker'
         """
+        pattern = r'(.*)%s(%s)(\d*)' % (self.SEPERATOR, '|'.join(map(re.escape, Operator.ALL)))
+        return re.findall(pattern, instruction)[0]
