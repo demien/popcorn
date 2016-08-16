05e7144b46e2d215b88abfb8647845deea1485b7
demien <demien@appannie.com>
2016-08-16 16:48:28 +0800

    WIP

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# install celery
 ln -s /var/popcorn/celery/celery /usr/local/bin/celery
 chmod 744 /var/popcorn/celery/celery
-export PYTHONPATH=/var/celery/
+
+# install popcorn
+pip install -e /var/popcorn
